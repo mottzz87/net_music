4b51d882fec18da6ba6300507af182794f329813
vane <dongxu.zhao@karakal.com.cn>
2019-11-22 11:02:06 +0800

    Updates

---
+++ precache-manifest.d45704cf682817b5554ee9c3788cd837.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cde6b9abce621f0a907f83af052833c",
+    "url": "/net_music/index.html"
+  },
+  {
+    "revision": "680b3302d8def838f03e",
+    "url": "/net_music/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "fd8d61c3dccc0257b800",
+    "url": "/net_music/static/js/2.1d4bce7d.chunk.js"
+  },
+  {
+    "revision": "680b3302d8def838f03e",
+    "url": "/net_music/static/js/main.f3084762.chunk.js"
+  },
+  {
+    "revision": "ec5a0d2dfd2a798ada85",
+    "url": "/net_music/static/js/runtime-main.45671689.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/net_music/static/media/logo.25bf045c.svg"
+  }
+]);
